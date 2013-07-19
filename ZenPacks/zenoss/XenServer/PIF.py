37e4ddafe03a5149340898912ba072b811384954
Chet Luther <cluther@zenoss.com>
2013-07-19 15:14:31 -0500

    Network: Fix lint and style issues.

--- ZenPacks/zenoss/XenServer/PIF.py
+++ ZenPacks/zenoss/XenServer/PIF.py
@@ -8,93 +8,56 @@
 #
 ######################################################################
 
+from zope.component import adapts
 from zope.interface import implements
-from Products.ZenModel.Device import Device
-from Products.ZenModel.ZenossSecurity import ZEN_CHANGE_DEVICE
-from Products.Zuul.decorators import info
+
+from Products.ZenRelations.RelSchema import ToMany, ToManyCont, ToOne
+from Products.Zuul.catalog.paths import DefaultPathReporter, relPath
 from Products.Zuul.form import schema
 from Products.Zuul.infos import ProxyProperty
 from Products.Zuul.utils import ZuulMessageFactory as _t
-from Products.ZenModel.DeviceComponent import DeviceComponent
-from Products.ZenModel.ManagedEntity import ManagedEntity
-from Products.Zuul.catalog.paths import DefaultPathReporter, relPath
-from Products.Zuul.infos.component import ComponentInfo
-from Products.Zuul.interfaces.component import IComponentInfo
-from Products.ZenRelations.RelSchema import ToMany, ToManyCont, ToOne
-from Products.ZenRelations.RelSchema import ToManyCont, ToOne
-from ZenPacks.zenoss.XenServer.utils import updateToOne
+
+from ZenPacks.zenoss.XenServer import CLASS_NAME, MODULE_NAME
+from ZenPacks.zenoss.XenServer.utils import (
+    BaseComponent, IBaseComponentInfo, BaseComponentInfo,
+    updateToOne,
+    )
 
 
-class PIF(DeviceComponent, ManagedEntity):
+class PIF(BaseComponent):
+    '''
+    Model class for PIF (physical interface.)
+    '''
     meta_type = portal_type = 'XenServerPIF'
-
-    Klasses = [DeviceComponent, ManagedEntity]
 
     IP = None
     MAC = None
     netmask = None
     gateway = None
-    uuid = None
 
-    _properties = ()
-    for Klass in Klasses:
-        _properties = _properties + getattr(Klass, '_properties', ())
-
-    _properties = _properties + (
+    _properties = BaseComponent._properties + (
         {'id': 'IP', 'type': 'string', 'mode': 'w'},
         {'id': 'MAC', 'type': 'string', 'mode': 'w'},
         {'id': 'netmask', 'type': 'string', 'mode': 'w'},
         {'id': 'gateway', 'type': 'string', 'mode': 'w'},
-        {'id': 'uuid', 'type': 'string', 'mode': 'w'},
         )
 
-    _relations = ()
-    for Klass in Klasses:
-        _relations = _relations + getattr(Klass, '_relations', ())
-
-    _relations = _relations + (
-        ('host', ToOne(ToManyCont, 'ZenPacks.zenoss.XenServer.Host', 'pifs',)),
-        ('network', ToOne(ToMany, 'ZenPacks.zenoss.XenServer.Network', 'pifs',)),
+    _relations = BaseComponent._relations + (
+        ('host', ToOne(ToManyCont, MODULE_NAME['Host'], 'pifs')),
+        ('network', ToOne(ToMany, MODULE_NAME['Network'], 'pifs')),
         )
 
-    factory_type_information = ({
-        'actions': ({
-            'id': 'perfConf',
-            'name': 'Template',
-            'action': 'objTemplates',
-            'permissions': (ZEN_CHANGE_DEVICE,),
-            },),
-        },)
-
-    def device(self):
-        '''
-        Return device under which this component/device is contained.
-        '''
-        obj = self
-
-        for i in range(200):
-            if isinstance(obj, Device):
-                return obj
-
-            try:
-                obj = obj.getPrimaryParent()
-            except AttributeError as exc:
-                raise AttributeError(
-                    'Unable to determine parent at %s (%s) '
-                    'while getting device for %s' % (
-                        obj, exc, self))
-
-    def getnetworkId(self):
+    def getNetwork(self):
         '''
         Return network id or None.
 
         Used by modeling.
         '''
-        obj = self.network()
-        if obj:
-            return obj.id
+        network = self.network()
+        if network:
+            return network.id
 
-    def setnetworkId(self, id_):
+    def setNetwork(self, network_id):
         '''
         Set network by id.
 
@@ -103,35 +66,45 @@ class PIF(DeviceComponent, ManagedEntity):
         updateToOne(
             relationship=self.network,
             root=self.device(),
-            type_='ZenPacks.zenoss.XenServer.Network',
-            id_=id_)
+            type_=CLASS_NAME['Network'],
+            id_=network_id)
 
 
-class IPIFInfo(IComponentInfo):
+class IPIFInfo(IBaseComponentInfo):
+    '''
+    API Info interface for PIF.
+    '''
 
     IP = schema.TextLine(title=_t(u'IPS'))
     MAC = schema.TextLine(title=_t(u'MACS'))
     netmask = schema.TextLine(title=_t(u'netmasks'))
     gateway = schema.TextLine(title=_t(u'gateways'))
-    uuid = schema.TextLine(title=_t(u'uuids'))
 
 
-class PIFInfo(ComponentInfo):
+class PIFInfo(BaseComponentInfo):
+    '''
+    API Info adapter factor for PIF.
+    '''
+
     implements(IPIFInfo)
+    adapts(PIF)
 
     IP = ProxyProperty('IP')
     MAC = ProxyProperty('MAC')
     netmask = ProxyProperty('netmask')
     gateway = ProxyProperty('gateway')
-    uuid = ProxyProperty('uuid')
 
 
 class PIFPathReporter(DefaultPathReporter):
+    '''
+    Path reporter for PIF.
+    '''
+
     def getPaths(self):
         paths = super(PIFPathReporter, self).getPaths()
 
-        obj = self.context.network()
-        if obj:
-            paths.extend(relPath(obj, 'endpoint'))
+        network = self.context.network()
+        if network:
+            paths.extend(relPath(network, 'endpoint'))
 
         return paths
