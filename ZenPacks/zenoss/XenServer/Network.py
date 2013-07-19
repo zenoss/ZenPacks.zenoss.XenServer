8b56667307ad3e272ffbc1878f226b3669ba11e0
Chet Luther <cluther@zenoss.com>
2013-07-19 14:47:48 -0500

    Network: Fix lint and style issues.

--- ZenPacks/zenoss/XenServer/Network.py
+++ ZenPacks/zenoss/XenServer/Network.py
@@ -8,170 +8,124 @@
 #
 ######################################################################
 
+from zope.component import adapts
 from zope.interface import implements
-from Products.ZenModel.Device import Device
-from Products.ZenModel.ZenossSecurity import ZEN_CHANGE_DEVICE
-from Products.Zuul.decorators import info
+
+from Products.ZenRelations.RelSchema import ToMany, ToManyCont, ToOne
 from Products.Zuul.form import schema
 from Products.Zuul.infos import ProxyProperty
 from Products.Zuul.utils import ZuulMessageFactory as _t
-from Products.ZenModel.DeviceComponent import DeviceComponent
-from Products.ZenModel.ManagedEntity import ManagedEntity
-from Products.Zuul.catalog.paths import DefaultPathReporter, relPath
-from Products.Zuul.infos.component import ComponentInfo
-from Products.Zuul.interfaces.component import IComponentInfo
-from Products.ZenRelations.RelSchema import ToMany, ToManyCont, ToOne
-from Products.ZenRelations.RelSchema import ToManyCont, ToOne
-from ZenPacks.zenoss.XenServer.utils import updateToMany
+
+from ZenPacks.zenoss.XenServer import CLASS_NAME, MODULE_NAME
+from ZenPacks.zenoss.XenServer.utils import (
+    BaseComponent, IBaseComponentInfo, BaseComponentInfo,
+    RelationshipLengthProperty,
+    updateToMany,
+    )
 
 
-class Network(DeviceComponent, ManagedEntity):
+class Network(BaseComponent):
+    '''
+    Model class for Network.
+    '''
+
     meta_type = portal_type = 'XenServerNetwork'
 
-    Klasses = [DeviceComponent, ManagedEntity]
-
-    bridge = None
-    uuid = None
-    tags = None
     name_label = None
+    name_description = None
+    bridge = None
+    tags = None
     MTU = None
     default_locking_mode = None
-    name_description = None
 
-    _properties = ()
-    for Klass in Klasses:
-        _properties = _properties + getattr(Klass, '_properties', ())
-
-    _properties = _properties + (
-        {'id': 'bridge', 'type': 'string', 'mode': 'w'},
-        {'id': 'uuid', 'type': 'string', 'mode': 'w'},
-        {'id': 'tags', 'type': 'string', 'mode': 'w'},
+    _properties = BaseComponent._properties + (
         {'id': 'name_label', 'type': 'string', 'mode': 'w'},
+        {'id': 'name_description', 'type': 'string', 'mode': 'w'},
+        {'id': 'bridge', 'type': 'string', 'mode': 'w'},
+        {'id': 'tags', 'type': 'string', 'mode': 'w'},
         {'id': 'MTU', 'type': 'string', 'mode': 'w'},
         {'id': 'default_locking_mode', 'type': 'string', 'mode': 'w'},
-        {'id': 'name_description', 'type': 'string', 'mode': 'w'},
         )
 
-    _relations = ()
-    for Klass in Klasses:
-        _relations = _relations + getattr(Klass, '_relations', ())
-
-    _relations = _relations + (
-        ('endpoint', ToOne(ToManyCont, 'ZenPacks.zenoss.XenServer.Endpoint', 'networks',)),
-        ('pifs', ToMany(ToOne, 'ZenPacks.zenoss.XenServer.PIF', 'network',)),
-        ('vifs', ToMany(ToOne, 'ZenPacks.zenoss.XenServer.VIF', 'network',)),
+    _relations = BaseComponent._relations + (
+        ('endpoint', ToOne(ToManyCont, MODULE_NAME['Endpoint'], 'networks')),
+        ('pifs', ToMany(ToOne, MODULE_NAME['PIF'], 'network')),
+        ('vifs', ToMany(ToOne, MODULE_NAME['VIF'], 'network')),
         )
 
-    factory_type_information = ({
-        'actions': ({
-            'id': 'perfConf',
-            'name': 'Template',
-            'action': 'objTemplates',
-            'permissions': (ZEN_CHANGE_DEVICE,),
-            },),
-        },)
-
-    def device(self):
+    def getPIFs(self):
         '''
-        Return device under which this component/device is contained.
-        '''
-        obj = self
-
-        for i in range(200):
-            if isinstance(obj, Device):
-                return obj
-
-            try:
-                obj = obj.getPrimaryParent()
-            except AttributeError as exc:
-                raise AttributeError(
-                    'Unable to determine parent at %s (%s) '
-                    'while getting device for %s' % (
-                        obj, exc, self))
-
-    def getPIFIds(self):
-        '''
-        Return a sorted list of each pif id related to this
-        Aggregate.
+        Return a sorted list of ids in pifs relationship.
 
         Used by modeling.
         '''
 
-        return sorted([pif.id for pif in self.pifs.objectValuesGen()])
+        return sorted(pif.id for pif in self.pifs.objectValuesGen())
 
-    def setPIFIds(self, ids):
+    def setPIFs(self, pif_ids):
         '''
-        Update PIF relationship given ids.
+        Update pifs relationship given ids.
 
         Used by modeling.
         '''
         updateToMany(
             relationship=self.pifs,
             root=self.device(),
-            type_='ZenPacks.zenoss.XenServer.PIF',
-            ids=ids)
+            type_=CLASS_NAME['PIF'],
+            ids=pif_ids)
 
-    def getVIFIds(self):
+    def getVIFs(self):
         '''
-        Return a sorted list of each vif id related to this
-        Aggregate.
+        Return a sorted list of ids in vifs relationship.
 
         Used by modeling.
         '''
 
-        return sorted([vif.id for vif in self.vifs.objectValuesGen()])
+        return sorted(vif.id for vif in self.vifs.objectValuesGen())
 
-    def setVIFIds(self, ids):
+    def setVIFs(self, vif_ids):
         '''
-        Update VIF relationship given ids.
+        Update vifs relationship given ids.
 
         Used by modeling.
         '''
         updateToMany(
             relationship=self.vifs,
             root=self.device(),
-            type_='ZenPacks.zenoss.XenServer.VIF',
-            ids=ids)
+            type_=CLASS_NAME['VIF'],
+            ids=vif_ids)
 
 
-class INetworkInfo(IComponentInfo):
+class INetworkInfo(IBaseComponentInfo):
+    '''
+    API Info interface for Network.
+    '''
+
+    name_label = schema.TextLine(title=_t(u'name_labels'))
+    name_description = schema.TextLine(title=_t(u'name_descriptions'))
+    bridge = schema.TextLine(title=_t(u'bridges'))
+    tags = schema.TextLine(title=_t(u'tag'))
+    MTU = schema.TextLine(title=_t(u'MTUS'))
+    default_locking_mode = schema.TextLine(title=_t(u'default_locking_modes'))
+
     pif_count = schema.Int(title=_t(u'Number of PIFS'))
     vif_count = schema.Int(title=_t(u'Number of VIFS'))
 
-    bridge = schema.TextLine(title=_t(u'bridges'))
-    uuid = schema.TextLine(title=_t(u'uuids'))
-    tags = schema.TextLine(title=_t(u'tag'))
-    name_label = schema.TextLine(title=_t(u'name_labels'))
-    MTU = schema.TextLine(title=_t(u'MTUS'))
-    default_locking_mode = schema.TextLine(title=_t(u'default_locking_modes'))
-    name_description = schema.TextLine(title=_t(u'name_descriptions'))
 
+class NetworkInfo(BaseComponentInfo):
+    '''
+    API Info adapter factory for Network.
+    '''
 
-class NetworkInfo(ComponentInfo):
     implements(INetworkInfo)
+    adapts(Network)
 
-    bridge = ProxyProperty('bridge')
-    uuid = ProxyProperty('uuid')
-    tags = ProxyProperty('tags')
     name_label = ProxyProperty('name_label')
+    name_description = ProxyProperty('name_description')
+    bridge = ProxyProperty('bridge')
+    tags = ProxyProperty('tags')
     MTU = ProxyProperty('MTU')
     default_locking_mode = ProxyProperty('default_locking_mode')
-    name_description = ProxyProperty('name_description')
 
-    @property
-    def pif_count(self):
-        # Using countObjects is fast.
-        try:
-            return self._object.pifs.countObjects()
-        except:
-            # Using len on the results of calling the relationship is slow.
-            return len(self._object.pifs())
-
-    @property
-    def vif_count(self):
-        # Using countObjects is fast.
-        try:
-            return self._object.vifs.countObjects()
-        except:
-            # Using len on the results of calling the relationship is slow.
-            return len(self._object.vifs())
+    pif_count = RelationshipLengthProperty('pifs')
+    vif_count = RelationshipLengthProperty('vifs')
