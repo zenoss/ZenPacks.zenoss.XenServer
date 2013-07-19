fccb96aac2ff3867212f8d6a44618351cf963337
Chet Luther <cluther@zenoss.com>
2013-07-19 15:23:54 -0500

    Modeler: Model PBD non-containing relationships.

--- ZenPacks/zenoss/XenServer/modeler/plugins/zenoss/XenServer.py
+++ ZenPacks/zenoss/XenServer/modeler/plugins/zenoss/XenServer.py
@@ -277,6 +277,7 @@ class XenServer(PythonPlugin, ModelerPluginCacheMixin):
             objmaps[properties['host']].append({
                 'id': id_from_ref(ref),
                 'title': title,
+                'setSR': id_from_ref(properties.get('SR')),
                 })
 
         for parent_ref, grouped_objmaps in objmaps.items():
