5abcd5b1b447d28c2014afea2788ac2874bbd0b0
Chet Luther <cluther@zenoss.com>
2013-07-30 14:14:08 -0500

    VDI: Cleanup schema.

--- ZenPacks/zenoss/XenServer/VDI.py
+++ ZenPacks/zenoss/XenServer/VDI.py
@@ -19,7 +19,7 @@ from Products.Zuul.utils import ZuulMessageFactory as _t
 from ZenPacks.zenoss.XenServer import CLASS_NAME, MODULE_NAME
 from ZenPacks.zenoss.XenServer.utils import (
     PooledComponent, IPooledComponentInfo, PooledComponentInfo,
-    RelationshipLengthProperty,
+    RelationshipInfoProperty, RelationshipLengthProperty,
     updateToMany,
     )
 
@@ -30,28 +30,34 @@ class VDI(PooledComponent):
     '''
     meta_type = portal_type = 'XenServerVDI'
 
-    name_label = None
+    allow_caching = None
+    allowed_operations = None
+    is_a_snapshot = None
+    location = None
+    managed = None
+    missing = None
     name_description = None
+    name_label = None
+    on_boot = None
     read_only = None
     sharable = None
-    missing = None
-    physical_utilisation = None
-    allow_caching = None
-    on_boot = None
-    virtual_size = None
-    Type = None
+    storage_lock = None
+    vdi_type = None
 
     _properties = PooledComponent._properties + (
-        {'id': 'name_label', 'type': 'string', 'mode': 'w'},
+        {'id': 'allow_caching', 'type': 'bool', 'mode': 'w'},
+        {'id': 'allowed_operations', 'type': 'lines', 'mode': 'w'},
+        {'id': 'is_a_snapshot', 'type': 'bool', 'mode': 'w'},
+        {'id': 'location', 'type': 'string', 'mode': 'w'},
+        {'id': 'managed', 'type': 'bool', 'mode': 'w'},
+        {'id': 'missing', 'type': 'bool', 'mode': 'w'},
         {'id': 'name_description', 'type': 'string', 'mode': 'w'},
-        {'id': 'read_only', 'type': 'string', 'mode': 'w'},
-        {'id': 'sharable', 'type': 'string', 'mode': 'w'},
-        {'id': 'missing', 'type': 'string', 'mode': 'w'},
-        {'id': 'physical_utilisation', 'type': 'string', 'mode': 'w'},
-        {'id': 'allow_caching', 'type': 'string', 'mode': 'w'},
-        {'id': 'on_boot', 'type': 'string', 'mode': 'w'},
-        {'id': 'virtual_size', 'type': 'string', 'mode': 'w'},
-        {'id': 'Type', 'type': 'string', 'mode': 'w'},
+        {'id': 'name_label', 'type': 'string', 'mode': 'w'},
+        {'id': 'on_boot', 'type': 'bool', 'mode': 'w'},
+        {'id': 'read_only', 'type': 'bool', 'mode': 'w'},
+        {'id': 'sharable', 'type': 'bool', 'mode': 'w'},
+        {'id': 'storage_lock', 'type': 'bool', 'mode': 'w'},
+        {'id': 'vdi_type', 'type': 'string', 'mode': 'w'},
         )
 
     _relations = PooledComponent._relations + (
@@ -86,18 +92,23 @@ class IVDIInfo(IPooledComponentInfo):
     API Info interface for VBD.
     '''
 
-    name_label = schema.TextLine(title=_t(u'name_labels'))
-    name_description = schema.TextLine(title=_t(u'name_descriptions'))
-    read_only = schema.TextLine(title=_t(u'read_onlies'))
-    sharable = schema.TextLine(title=_t(u'sharables'))
-    missing = schema.TextLine(title=_t(u'missings'))
-    physical_utilisation = schema.TextLine(title=_t(u'physical_utilisations'))
-    allow_caching = schema.TextLine(title=_t(u'allow_cachings'))
-    on_boot = schema.TextLine(title=_t(u'on_boots'))
-    virtual_size = schema.TextLine(title=_t(u'virtual_sizes'))
-    Type = schema.TextLine(title=_t(u'Types'))
+    sr = schema.Entity(title=_t(u'Storage Repository'))
 
-    vbd_count = schema.Int(title=_t(u'Number of VBDS'))
+    allow_caching = schema.Bool(title=_t(u'Allow Caching'))
+    allowed_operations = schema.Text(title=_t(u'Allowed Operations'))
+    is_a_snapshot = schema.Bool(title=_t(u'Is a Snapshot'))
+    location = schema.TextLine(title=_t(u'Location'))
+    managed = schema.Bool(title=_t(u'Managed'))
+    missing = schema.Bool(title=_t(u'Missing'))
+    name_description = schema.TextLine(title=_t(u'Description'))
+    name_label = schema.TextLine(title=_t(u'Label'))
+    on_boot = schema.Bool(title=_t(u'On Boot'))
+    read_only = schema.Bool(title=_t(u'Read Only'))
+    sharable = schema.Bool(title=_t(u'Sharable'))
+    storage_lock = schema.Bool(title=_t(u'Storage Lock'))
+    vdi_type = schema.TextLine(title=_t(u'Type'))
+
+    vbd_count = schema.Int(title=_t(u'Number of Virtual Block Devices'))
 
 
 class VDIInfo(PooledComponentInfo):
@@ -108,15 +119,20 @@ class VDIInfo(PooledComponentInfo):
     implements(IVDIInfo)
     adapts(VDI)
 
-    name_label = ProxyProperty('name_label')
+    sr = RelationshipInfoProperty('sr')
+
+    allow_caching = ProxyProperty('allow_caching')
+    allowed_operations = ProxyProperty('allowed_operations')
+    is_a_snapshot = ProxyProperty('is_a_snapshot')
+    location = ProxyProperty('location')
+    managed = ProxyProperty('managed')
+    missing = ProxyProperty('missing')
     name_description = ProxyProperty('name_description')
+    name_label = ProxyProperty('name_label')
+    on_boot = ProxyProperty('on_boot')
     read_only = ProxyProperty('read_only')
     sharable = ProxyProperty('sharable')
-    missing = ProxyProperty('missing')
-    physical_utilisation = ProxyProperty('physical_utilisation')
-    allow_caching = ProxyProperty('allow_caching')
-    on_boot = ProxyProperty('on_boot')
-    virtual_size = ProxyProperty('virtual_size')
-    Type = ProxyProperty('Type')
+    storage_lock = ProxyProperty('storage_lock')
+    vdi_type = ProxyProperty('vdi_type')
 
     vbd_count = RelationshipLengthProperty('vbds')
