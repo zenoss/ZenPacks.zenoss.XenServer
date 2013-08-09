973d5f670474a9df104ea280b64436855f00b98d
Chet Luther <chet.luther@gmail.com>
2013-08-09 18:41:16 -0500

    Require PythonCollector>=1.1 for modeling support.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.XenServer']
-INSTALL_REQUIRES = ['ZenPacks.zenoss.PythonCollector']
+INSTALL_REQUIRES = ['ZenPacks.zenoss.PythonCollector>=1.1']
 COMPAT_ZENOSS_VERS = ">=4.2"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
