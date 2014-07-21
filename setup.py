76e5c1a667a1f925ae68f61065d6ee25d9fa28fe
Chet Luther <chet.luther@gmail.com>
2014-07-21 09:27:56 -0500

    Post Release: Version 1.0.2 -> 1.0.3dev.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.XenServer"
-VERSION = "1.0.2"
+VERSION = "1.0.3dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
